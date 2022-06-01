42b94b02ac89f00b0adc8bf44c816c0b9a57dcde
Exigoll <exigoll@bk.ru>
2022-06-01 15:05:26 +0500

    feat: structure project + eslint / prettier / hasky

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// eslint-disable-next-line react/function-component-definition
+const Calculator: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Calculator;
